722da850a2693ffc90471acbf03923802813f057
GitElean <eleanjulian@gmail.com>
2021-05-20 07:12:24 -0600

    ´20/05
    sefini

---
+++ Lab7/Assets/Scripts/NavAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgent : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(mouseRay, out hitInfo))
+            {
+                NavMeshHit navhit;
+                if(NavMesh.SamplePosition(hitInfo.point, out navhit, 0.2f, NavMesh.AllAreas))
+                {
+                    agent.SetDestination(navhit.position);
+                    print(navhit.position);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("mob"))
+            Destroy(gameObject);
+    }
+}
---
+++ Lab7/Assets/Scripts/Patrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Patrol : MonoBehaviour
+{
+    public Transform wpCont;
+
+    List<Transform> waypoints;//lista de waypoints
+
+    int cWP = 0;//waypoint actual
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        waypoints = new List<Transform>();
+
+        //añadir los puntos a la lista a recorrer
+        foreach (Transform child in wpCont)
+            waypoints.Add(child);
+
+        agent.SetDestination(waypoints[cWP].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //recorrer los waypoints en orden
+        if(agent.remainingDistance < 0.2f)
+        {
+            cWP++;
+            cWP = cWP % waypoints.Count;
+            agent.SetDestination(waypoints[cWP].position);
+
+        }
+    }
+}
